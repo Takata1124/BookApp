2c18b241130a99fd3f91a865d32ab4efb9149061
Takata1124 <skkyosk1124@gmail.com>
2024-01-04 00:24:09 +0900

    Initial Commit

---
+++ practiceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practiceApp
+//
+//  Created by t032fj on 2024/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
